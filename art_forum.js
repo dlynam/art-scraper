4f499a78464741b3c005d84584f0534fe5a7090f
Dave Lynam <dlynam@gmail.com>
2013-02-05 12:55:50 -0800

    update

--- art_forum.js
+++ art_forum.js
@@ -1,23 +1,46 @@
-exports.scrape = function(){
+exports.scrape = function(){ 
   require('node.io').scrape(function() {
       this.getHtml('http://www.artforum.com/picks/section=us', function(err, $) {
-          var names = [];
+          var entries = [];
 				  $('div.Item').each(function(item) {
+						//var entry = {};
 						var dateText = $('h3', item).text;
-						//console.log(dateText);	
-					  extractDate(dateText);
-						$('p').each(function(p) {
-							//console.log(p.text);
+					  var entryDate = extractDate(dateText);
+						console.log(entryDate);
+						$('a', item).each(function(link) {
+							extractNames(link.text);
+						})
+						$('p', item).each(function(p) {
+							extractNames(p.text);
 						});
           });
-          this.emit(names);
       });
   });
 }
-
+		
 function extractDate(dateText){
 	var months = "january|february|march|april|may|june|july|august|september|november|december";
   var re = new RegExp("(" + months + ")\\s*\\d*", "i"); 
-	d = dateText.match(re);
-  console.log(d[0]);
+	d = dateText.match(re)[0] + ", 2012";
+	return d;
 }
+
+function extractNames(text){
+  var names = [];
+	names = text.match(/[A-Z][-'a-zA-Z]+\s{1}[A-Z][-'a-zA-Z]+/g)
+  if(names != null){
+		console.log(names[0]);
+  }
+}
+
+  //  /[A-Z]+\s{1}[A-Z]+/  all caps check
+
+  // return names;
+	//console.log(names[0]);
+	//for(i=0;i<names.length; i++){
+  //  console.log(names[i]);
+	//}
+
+
+
+
