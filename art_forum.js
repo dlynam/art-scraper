1686c3e3904b1ea0e040cd18ec4b991221884df6
Dave <dlynam@gmail.com>
2013-02-06 14:53:12 -0800

    update

--- art_forum.js
+++ art_forum.js
@@ -1,46 +1,82 @@
 exports.scrape = function(){ 
   require('node.io').scrape(function() {
-      this.getHtml('http://www.artforum.com/picks/section=us', function(err, $) {
-          var entries = [];
-				  $('div.Item').each(function(item) {
-						//var entry = {};
-						var dateText = $('h3', item).text;
-					  var entryDate = extractDate(dateText);
-						console.log(entryDate);
-						$('a', item).each(function(link) {
-							extractNames(link.text);
-						})
-						$('p', item).each(function(p) {
-							extractNames(p.text);
-						});
-          });
+    this.getHtml('http://www.artforum.com/picks/section=us', function(err, $) {
+      $('div.Item').each(function(item) {
+        var dateText = $('h3', item).text;
+        var entryDate = extractDate(dateText);
+        $('a', item).each(function(link) {
+          var names = extractNames(link.text);
+          mongoSave(names);
+        });
+        $('p', item).each(function(p) {
+          var names = extractNames(p.text);
+          mongoSave(names);
+        });
       });
+    });
   });
 }
 		
 function extractDate(dateText){
-	var months = "january|february|march|april|may|june|july|august|september|november|december";
-  var re = new RegExp("(" + months + ")\\s*\\d*", "i"); 
-	d = dateText.match(re)[0] + ", 2012";
+	d = dateText.match(dateRegex())[0] + ", 2012";
+  console.log("");
+  console.log(d);
 	return d;
+}
+
+function mongoSave(names){
+  var commonRegex = commonWordsRegex();
+	for(i=0;i<names.length; i++){
+    var name = names[i];
+    if(capsCheck(name) == null){
+      var testCommon = name.match(commonRegex);
+      //console.log(testCommon)
+      if(testCommon == null){
+        console.log(name)
+      }
+    }
+	}
 }
 
 function extractNames(text){
   var names = [];
 	names = text.match(/[A-Z][-'a-zA-Z]+\s{1}[A-Z][-'a-zA-Z]+/g)
   if(names != null){
-		console.log(names[0]);
+    return names;
+  } else {
+    return [];
   }
 }
 
-  //  /[A-Z]+\s{1}[A-Z]+/  all caps check
+function capsCheck(name){
+  var check = name.match(/^[A-Z]+\s{1}[A-Z]+/)
+  return check;
+}
 
-  // return names;
-	//console.log(names[0]);
-	//for(i=0;i<names.length; i++){
-  //  console.log(names[i]);
-	//}
+function commonWordsRegex(){
+  var fs = require('fs');
+  commonWords = fs.readFileSync('common_words.txt').toString().split("\n");
+  removeBlankNewlines(commonWords);
+  var commonString = commonWords.join("|")
+  var re = new RegExp(commonWords.join("|"), "i");
+  return re;
+}
 
+function dateRegex(){
+	var months = "january|february|march|april|may|june|july|august|september|november|december";
+  var re = new RegExp("(" + months + ")\\s*\\d*", "i"); 
+  return re;
+}
 
-
-
+//removes blank lines at end of common_words.txt
+function removeBlankNewlines(commonWords){
+  var i = commonWords.length-1;
+  while(i > 0){
+    if(commonWords[i].match(/[a-z]/i) == null){
+      commonWords.splice(i,1);
+      i--;
+    } else {
+      break;
+    }
+  }
+}
--- run_scraper.js
+++ run_scraper.js
@@ -3,3 +3,4 @@ nodeIO = require("node.io")
 
 var art_forum = require('./art_forum.js');
 art_forum.scrape();
+
